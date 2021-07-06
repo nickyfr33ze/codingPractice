6435086282b7996beae151cfec781f209f00f4e6
Nicholas Friesen <nfriesen26@gmail.com>
2021-07-05 23:42:14 -0700

    created a java folder and a hello world file

---
+++ java/helloWorld.java
@@ -0,0 +1,12 @@
+//Written by Nick Friesen
+//
+//July 5th 2021
+// Basic Hello World!
+
+class Java{
+        public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
+
