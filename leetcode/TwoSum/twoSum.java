138e02422a667a7a14371d884452858f3b08106d
Nicholas Friesen <nfriesen26@gmail.com>
2021-08-03 11:45:51 -0700

    added a section for leetcode tests. fin twoSum

---
+++ leetcode/TwoSum/twoSum.java
@@ -0,0 +1,13 @@
+class TwoSum{
+    public static void twoSum(int num1, int num2){
+        int count = 0;
+        int sum = num1 + num2;
+        System.out.println("["+num1+"],["+num2+"]");
+        System.out.println(sum);
+    }
+    
+    public static void main(String[] args){
+        twoSum(10,5);
+    }
+    
+}
