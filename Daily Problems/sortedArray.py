38cd3e4ace308ea1666c689bf3ca374a247af156
Nick Friesen <nfriesen26@gmail.com>
2022-01-30 12:38:47 -0800

    attempt  1

--- Daily Problems/sortedArray.py
+++ Daily Problems/sortedArray.py
@@ -26,11 +26,14 @@ print(Solution().getRange(arr, x))
 '''
 class Solution:
     def getRange(self, arr, target):
+        arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]    # Input
+        x = 2                                   # Target
         #Fill this in
-        
+        count = 0
+        while(int i = 0, i < arr.size, i++):    # iterate over the array and confirm that the pointer 'i' is less than the array size and increase it by one
+            while(int j = 0, j < arr[i], j++):  # compare 'j' to the position of 'i' in the array and increase it by 1 
 
+            
 
         #Test the program   this is all currently in the function. Does this need to be outside the function?
-        arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8]
-        x = 2
         print(Solution().getRange(arr, x))
