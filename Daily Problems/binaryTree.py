290e4c72e70c4477783e4ee4bc04656b0977d3ed
Nicholas Friesen <nfriesen26@gmail.com>
2022-02-08 16:17:29 -0800

    worked on the binary tree

--- Daily Problems/binaryTree.py
+++ Daily Problems/binaryTree.py
@@ -14,7 +14,7 @@ class Node:
 def findCeilingFloor(root_node, k, floor=None, ceil=None):
   # Fill this in.
 
-root = Node(8) 
+root = Node(8)              Draw this out. This is where the binary tree is being created
 root.left = Node(4) 
 root.right = Node(12) 
   
@@ -26,4 +26,27 @@ root.right.right = Node(14)
 
 print findCeilingFloor(root, 5)
 # (4, 6)
+Help : https://www.geeksforgeeks.org/floor-and-ceil-from-a-bst/
 '''
+
+class Node:
+  def __init__(self,value):
+    self.left = None
+    self.right = None
+    self.value = value
+
+def findCeilingFLoor(root_node, k, floor=None, ceil=None):
+  if root_node < 0:
+        print("Invalid value. Please re-run and enter a positive value.")
+  elif root_node >= 1:
+        # continue the program
+        if root_node > ceil:
+              #stop the program as this is would break the program
+              print("Stopping program. Invalid entry.")
+        elif         
+
+"""
+root = Node(8)
+root.left.left = Node(2)
+root.left.right = Node(12)
+"""
