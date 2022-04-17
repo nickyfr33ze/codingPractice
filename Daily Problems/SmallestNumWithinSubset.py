f4853853f0cc999d019f6fa8eb949c737607925f
Nick Friesen <nfriesen26@gmail.com>
2022-04-17 09:06:18 -0700

    began work

--- Daily Problems/SmallestNumWithinSubset.py
+++ Daily Problems/SmallestNumWithinSubset.py
@@ -11,7 +11,21 @@ Output: 7
 Numbers 1 to 6 can all be summed by a subset of the list of numbers, but 7 cannot.
 """
 def findSmallest(nums):
-  # 
+    print("The numbers we are working with are printed below:")
+    print(nums)
+    print("-----------------------------")
+    count = 0
+    num1 = 0
+    num2 = 0
+    tempSum = 0
+    while nums.length > 0 & count < nums.length: # This will continue looping until we reach the end of the length of numbers
+        nums[count] += num1
+        nums[count + 1] += num2
+        tempSum = num1 + num2
+        # Now we need a way to compare the tempSum to the rest of the sums of all the other numbers in the array passed in
+        # There has to be a more effective way to do this...
+        # Do some research on algorithms to best find this solution in an effective amount of time
+
 
 print(findSmallest([1, 2, 3, 8, 9, 10]))
 # 7
